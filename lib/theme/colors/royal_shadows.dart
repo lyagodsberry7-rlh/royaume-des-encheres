2e7e4681b99e6ba107f025317b224a9312de3863
lyagodsberry7-rlh <lyagodsberry7@gmail.com>
2026-08-05 17:39:47 +0000

    Fix: replace withValues(...) with withOpacity(...) in royal_shadows.dart

--- lib/theme/colors/royal_shadows.dart
+++ lib/theme/colors/royal_shadows.dart
@@ -5,7 +5,7 @@ class RoyalShadows {
 
   static final List<BoxShadow> small = [
     BoxShadow(
-      color: Colors.black.withValues(alpha: .15),
+      color: Colors.black.withOpacity(0.15),
       blurRadius: 8,
       offset: const Offset(0, 4),
     ),
@@ -13,7 +13,7 @@ class RoyalShadows {
 
   static final List<BoxShadow> medium = [
     BoxShadow(
-      color: Colors.black.withValues(alpha: .22),
+      color: Colors.black.withOpacity(0.22),
       blurRadius: 16,
       offset: const Offset(0, 8),
     ),
@@ -21,7 +21,7 @@ class RoyalShadows {
 
   static final List<BoxShadow> large = [
     BoxShadow(
-      color: Colors.black.withValues(alpha: .30),
+      color: Colors.black.withOpacity(0.30),
       blurRadius: 26,
       offset: const Offset(0, 14),
     ),
@@ -29,7 +29,7 @@ class RoyalShadows {
 
   static final List<BoxShadow> glow = [
     BoxShadow(
-      color: const Color(0xFFFFC107).withValues(alpha: .25),
+      color: const Color(0xFFFFC107).withOpacity(0.25),
       blurRadius: 20,
       spreadRadius: 1,
     ),
